2c92d3d1ebb05c6909c2dedf837f38405f30a5de
Talantseva Ekaterina <talekaterina@gmail.com>
2017-06-05 16:47:03 +0300

    Add files via upload

---
+++ Lab 5 Game Life/life.py
@@ -0,0 +1,53 @@
+import pygame
+from pygame.locals import *
+
+
+class GameOfLife:
+    def __init__(self, width = 640, height = 480, cell_size = 10, speed = 10):
+        self.width = width
+        self.height = height
+        self.cell_size = cell_size
+
+        # Устанавливаем размер окна
+        self.screen_size = width, height
+        # Создание нового окна
+        self.screen = pygame.display.set_mode(self.screen_size)
+
+        # Вычисляем количество ячеек по вертикали и горизонтали
+        self.cell_width = self.width // self.cell_size
+        self.cell_height = self.height // self.cell_size
+
+        # Скорость протекания игры
+        self.speed = speed
+
+
+   def draw_grid(self):
+        # http://www.pygame.org/docs/ref/draw.html#pygame.draw.line
+        for x in range(0, self.width, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                (x, 0), (x, self.height))
+        for y in range(0, self.height, self.cell_size):
+            pygame.draw.line(self.screen, pygame.Color('black'),
+                (0, y), (self.width, y))
+
+
+   def run(self):
+        pygame.init()
+
+        clock = pygame.time.Clock()
+        pygame.display.set_caption('Game of Life')
+        self.screen.fill(pygame.Color('white'))
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = False
+            self.draw_grid()
+            pygame.display.flip()
+            clock.tick(self.speed)
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    game = GameOfLife(320, 240, 20)
+    game.run()
